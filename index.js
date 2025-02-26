782c2238beef22a5caf0b486bc9355a9a6b742b8
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-02-26 20:11:55 +0530

    Port bug

--- index.js
+++ index.js
@@ -213,11 +213,5 @@ async function generateContent(companyName, productName, idealUser) {
   }
 }
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 
 export default app;
